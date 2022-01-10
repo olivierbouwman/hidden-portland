116754d84ff148f0d3ecb998b29c48fb0a0fa9a6
Olivier Bouwman <olivier@thinkshout.com>
2022-01-09 20:44:32 -0800

    scrape tweaks

--- scrape-infinite-scroll.js
+++ scrape-infinite-scroll.js
@@ -2,24 +2,23 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 
+// note: trying manually in brower using this code to scroll down:
+// for(var i = 0; i < 100; i++){ setTimeout(() => {window.scrollTo(0, document.body.scrollHeight);}, i*1000);}
+// also: Type about:config in the address bar and press Enter (accept the risk, if asked) Type in the search bar : permissions.default.image and set its value to 2 (was 1)
+
+// Read our existing post data so we can update/add.
+// let rawdata = fs.readFileSync('data.json');
+// let posts = JSON.parse(rawdata);
+
 function extractItems() {
-    /*  For extractedElements, you are selecting the tag and class,
-        that holds your desired information,
-        then choosing the desired child element you would like to scrape from.
-        in this case, you are selecting the
-        "<div class=blog-post />" from "<div class=container />" See below: */
-    // const scrapeBlocker = Array.from(document.querySelectorAll('h2')).find(el => el.textContent === 'Not Logged In');
-    // if (typeof scrapeBlocker !== "undefined") {console.log('scrape blocked!');}
-    const extractedElements = document.querySelectorAll('[role="feed"] .du4w35lb.k4urcfbm.l9j0dhe7.sjgh65i0');
     const items = [];
     let text;
     let likes;
     let link;
-    for (let element of extractedElements) {
-        // text = element.querySelectorAll('[style="text-align:start"], [style="text-align: start;"]');
-        text = element.querySelectorAll('[data-ad-preview="message"]');
-        likes = element.querySelectorAll('.pcp91wgn');
-        link = element.querySelectorAll('a[href^="https://www.facebook.com/groups/Hiddenportlandforthecurious/posts/"]');
+    document.querySelectorAll('[role="feed"] .du4w35lb.k4urcfbm.l9j0dhe7.sjgh65i0').forEach(function( node ) {
+        text = node.querySelectorAll('[data-ad-preview="message"]');
+        likes = node.querySelectorAll('.pcp91wgn');
+        link = node.querySelectorAll('a[href^="https://www.facebook.com/groups/Hiddenportlandforthecurious/posts/"]');
         const textCombined = [];
         for (let textElement of text) {
             textCombined.push(textElement.innerText);
@@ -30,11 +29,16 @@ function extractItems() {
             link: link[0].href.split('?')[0],
             date: link[0].innerText,
         };
+        // See if this post is already in our source data.
+        // for (var i=0 ; i < post.list.length ; i++) {
+        //     if (obj.post[i][searchField] == searchVal) {
+        //         results.push(obj.list[i]);
+        //     }
+        // }
         items.push(post);
-    }
-    // Try to keep the dom from getting too big, guessing that is what eventually crashes the browser.
-    Array.prototype.forEach.call( extractedElements, function( node ) {
+        // Try to keep the dom from getting too big, guessing that is what eventually crashes the browser.
         node.parentNode.removeChild( node );
+        // node.parentNodeinnerHTML
     });
     return items;
 }
@@ -43,7 +47,7 @@ async function scrapeItems(
     page,
     extractItems,
     itemCount,
-    scrollDelay = 0,
+    scrollDelay = 1000,
     // scrollDelay = Math.random() * (2000 - 250) + 250,
 ) {
     let items = [];
@@ -54,10 +58,19 @@ async function scrapeItems(
             items = items.concat(itemsNew);
             previousHeight = await page.evaluate('document.body.scrollHeight');
             await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
-            await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
+            try {
+                await page.waitForFunction(`document.body.scrollHeight != ${previousHeight}`, {timeout: 20000});
+            } catch (err) {
+                console.log('waiting for new posts timed out.');
+            }
             await page.waitForTimeout(scrollDelay);
             fs.writeFileSync('./data.json', JSON.stringify(items, null, 2) , 'utf-8');
             console.log(items.length);
+            // await page._client.send('HeapProfiler.enable');
+            // await page._client.send('HeapProfiler.collectGarbage');
+            // await page._client.send('HeapProfiler.disable');
+            // await page.evaluate(() => gc());
+            // page.dispose(); // try to fix memory issue. Probably related to V8 4GB limit.
         }
     } catch(e) { }
     return items;
@@ -67,15 +80,16 @@ async function scrapeItems(
     // Set up Chromium browser and page.
     const browser = await puppeteer.launch({
         headless: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--js-flags=--expose-gc', '--js-flags="--max-old-space-size=8192"', '--js-flags="--max-old-space-size=8192"'],
         // devtools: true,
     });
     const page = await browser.newPage();
-    page.setViewport({ width: 1280, height: 1000 });
+    page.setViewport({ width: 1280, height: 5000 });
     // Don't download images
     await page.setRequestInterception(true);
     page.on('request', (req) => {
-        if (req.resourceType() === 'image') {
+        // if (req.resourceType() === 'stylesheet' || req.resourceType() === 'image' || req.resourceType() === 'font') {
+        if (req.resourceType() === 'media' || req.resourceType() === 'image' || req.resourceType() === 'font') {
             req.abort();
         } else {
             req.continue();
@@ -84,15 +98,16 @@ async function scrapeItems(
 
     // Navigate to the example page.
     await page.goto('https://www.facebook.com/groups/Hiddenportlandforthecurious?sorting_setting=CHRONOLOGICAL');
-    
+
+    // If we want to get a lot of old posts, we need a facebook user, otherwise we get blocked after something like 168 posts.
     const cookies = [
         {
             'name': 'xs',
-            'value': '26%3AY6O01Y759DT_dg%3A2%3A1639810573%3A-1%3A-1'
+            'value': '46%3AwsKioNVhrWXJqw%3A2%3A1641630689%3A-1%3A-1'
         },
         {
             'name': 'c_user',
-            'value': '100076084785759'
+            'value': '100076737799177'
         }
     ];
 
