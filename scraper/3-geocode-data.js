6bad1530654adb97e61a3643b04024143d382758
Olivier Bouwman <olivier@thinkshout.com>
2022-01-20 23:02:39 -0800

    scraper updates

--- scraper/3-geocode-data.js
+++ scraper/3-geocode-data.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const https = require('https')
-var url_prefix = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+var url_prefix = 'https://maps.googleapis.com/maps/api/geocode/json?components=locality%3APortland%7Cadministrative_area%3AOR%7Ccountry%3AUS&address='
 var url_suffix = ', Portland, OR&key=' + process.env.GOOGLE_MAPS_API_KEY;
 
-let rawdata = fs.readFileSync('data.json');
+let rawdata = fs.readFileSync('data-efiles.json');
 let posts = JSON.parse(rawdata);
 let counter = 0;
 let counterFail = 0;
@@ -11,14 +11,22 @@ let counterFail = 0;
 async function run() {
     for (let post of posts) {
         // Only process if it hasn't been before and also process a certain date to prevent accidental high cost.
-        if (post.text.length > 5 && post.latlong == undefined && post.date.substring(0,4) == "2019") {
+        if (post.text.length > 5 && post.latlong == undefined) { // && post.date.substring(0,4) == "2021") {
             counter = counter + 1
             let text = post.text;
-            // Filter html tags
-            text = text.replace(/<\/?[^>]+(>|$)/g, "");
             // Filter efiles specific way of writing.
-            text = text.replace(/ looking (north|northwest|northeast|south|southwest|southeast|east|west) (to|towards) /gi, ' '); // is this case sensitive? we don't want it to.
+            // if (post.source == "efiles") {
+                // Remove ID.
+                text = text.replace(/A\d\d\d\d-\d+\.\d+ ?:/g, "");
+                text = text.replace(/A\d\d\d\d-\d+\. ?:/g, "");
+                // Filter html tags
+                text = text.replace(/<\/?[^>]+(>|$)/g, ", ");
+                text = text.replace(/ looking (north|northwest|northeast|south|southwest|southeast|east|west) (to|towards|from) /gi, ' & ');
+                text = text.replace(/ just (north|northwest|northeast|south|southwest|southeast|east|west) (of) /gi, ' & ');
+            // }
+            // if (post.source == "hp") {
             text = text.replace(/… More/g, ' ');
+            // }
             text = text.replace(/#/g, ', ');
             text = text.replace(/\n/g, ', ');
             if (!text.includes("&")) {
@@ -32,7 +40,7 @@ async function run() {
             const url = url_prefix + encodeURIComponent(text) + encodeURI(url_suffix);
             post = await processRecord(post, url);
             let aatemp = post.latlong.lat + ", " + post.latlong.lng;
-            fs.writeFileSync('data.json', JSON.stringify(posts, null, 2) , 'utf-8')
+            fs.writeFileSync('data-efiles.json', JSON.stringify(posts, null, 2) , 'utf-8')
         }
     }
     console.log("Geocoded " + counter + " items, of which " + counterFail + " could not be geocoded.");
